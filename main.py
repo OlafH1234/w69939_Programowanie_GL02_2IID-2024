a0eabd35b311009a19c7d5dfb1a7b239b0bd4a2a
Olaf Hocheker <w69939@student.wsiz.edu.pl>
2024-03-15 11:58:31 +0100

    lab2

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1
+# import math
+# def szescian(x):
+#     y = (math.pow(x,3))
+#     potega = y
+#     mod = potega % 10
+#     return mod
+# do_szesc =(int(input("podaj liczbe")))
+# wynik = szescian(do_szesc)
+# print("liczba", do_szesc, "podniesiona do szescianu to", wynik)
+# 2
+
