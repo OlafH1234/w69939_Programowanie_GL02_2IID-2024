45ff0fc04761907d16a3c76a845655c8044353f8
Olaf Hocheker <w69939@student.wsiz.edu.pl>
2024-05-10 11:50:05 +0200

    init

--- skrypt.js
+++ skrypt.js
@@ -5,39 +5,68 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     }
 });
 
+document.getElementById('sameAddress').addEventListener('change', function() {
+    const correspondenceAddressContainer = document.getElementById('correspondenceAddressContainer');
+    const correspondenceAddressInput = document.getElementById('correspondenceAddress');
+    
+    if (this.checked) {
+        correspondenceAddressInput.value = ''; // Wyczyść adres korespondencyjny
+        correspondenceAddressContainer.style.display = 'none'; // Ukryj pole adresu korespondencyjnego
+        correspondenceAddressInput.disabled = true; // Ustaw pole adresu korespondencyjnego jako nieaktywne
+    } else {
+        correspondenceAddressContainer.style.display = 'block'; // Wyświetl pole adresu korespondencyjnego
+        correspondenceAddressInput.disabled = false; // Ustaw pole adresu korespondencyjnego jako aktywne
+    }
+});
+
+document.getElementById('country').addEventListener('change', function() {
+    const provinceInput = document.getElementById('province');
+    if (this.value === 'Polska') {
+        provinceInput.innerHTML = `
+            <option value="">Wybierz</option>
+            <option value="dolnośląskie">Dolnośląskie</option>
+            <option value="kujawsko-pomorskie">Kujawsko-Pomorskie</option>
+            <!-- inne województwa -->
+        `;
+        provinceInput.disabled = false; // Ustaw pole województwa jako aktywne
+    } else {
+        provinceInput.innerHTML = `<option value="">Wybierz</option>`; // Usuń opcje województw
+        provinceInput.disabled = true; // Ustaw pole województwa jako nieaktywne
+    }
+});
+
+document.getElementById('phone').addEventListener('input', function() {
+    this.value = this.value.replace(/\D/g, ''); // Usuń wszystkie znaki nie będące cyframi
+});
+
 function validateForm() {
     let isValid = true;
 
-    const firstName = document.getElementById('firstName');
-    const lastName = document.getElementById('lastName');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const confirmPassword = document.getElementById('confirmPassword');
-    const gender = document.getElementById('gender');
-    const phone = document.getElementById('phone');
-    const birthdate = document.getElementById('birthdate');
-    const country = document.getElementById('country');
-
-    isValid &= checkRequired(firstName, 'firstNameError');
-    isValid &= checkRequired(lastName, 'lastNameError');
-    isValid &= checkRequired(email, 'emailError');
-    isValid &= checkEmail(email, 'emailError');
-    isValid &= checkRequired(password, 'passwordError');
-    isValid &= checkLength(password, 8, 'passwordError');
-    isValid &= checkRequired(confirmPassword, 'confirmPasswordError');
-    isValid &= checkPasswordMatch(password, confirmPassword, 'confirmPasswordError');
-    isValid &= checkRequired(gender, 'genderError');
-    isValid &= checkRequired(phone, 'phoneError');
-    isValid &= checkPhone(phone, 'phoneError');
-    isValid &= checkRequired(birthdate, 'birthdateError');
-    isValid &= checkBirthdate(birthdate, 'birthdateError');
-    isValid &= checkRequired(country, 'countryError');
+    isValid &= checkRequired('firstName');
+    isValid &= checkRequired('lastName');
+    isValid &= checkRequired('email');
+    isValid &= checkEmail('email');
+    isValid &= checkRequired('password');
+    isValid &= checkLength('password', 8);
+    isValid &= checkRequired('confirmPassword');
+    isValid &= checkPasswordMatch('password', 'confirmPassword');
+    isValid &= checkRequired('gender');
+    isValid &= checkRequired('phone');
+    isValid &= checkPhone('phone');
+    isValid &= checkRequired('birthdate');
+    isValid &= checkBirthdate('birthdate');
+    isValid &= checkRequired('country');
+    
+    if (!document.getElementById('sameAddress').checked) {
+        isValid &= checkRequired('correspondenceAddress');
+    }
 
     return isValid;
 }
 
-function checkRequired(input, errorId) {
-    const error = document.getElementById(errorId);
+function checkRequired(inputId) {
+    const input = document.getElementById(inputId);
+    const error = input.nextElementSibling;
     if (input.value.trim() === '') {
         error.textContent = 'To pole jest wymagane';
         return false;
@@ -47,8 +76,9 @@ function checkRequired(input, errorId) {
     }
 }
 
-function checkLength(input, minLength, errorId) {
-    const error = document.getElementById(errorId);
+function checkLength(inputId, minLength) {
+    const input = document.getElementById(inputId);
+    const error = input.nextElementSibling;
     if (input.value.trim().length < minLength) {
         error.textContent = `To pole musi zawierać co najmniej ${minLength} znaków`;
         return false;
@@ -58,8 +88,9 @@ function checkLength(input, minLength, errorId) {
     }
 }
 
-function checkEmail(input, errorId) {
-    const error = document.getElementById(errorId);
+function checkEmail(inputId) {
+    const input = document.getElementById(inputId);
+    const error = input.nextElementSibling;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(input.value.trim())) {
         error.textContent = 'Podaj poprawny adres email';
@@ -70,8 +101,10 @@ function checkEmail(input, errorId) {
     }
 }
 
-function checkPasswordMatch(password, confirmPassword, errorId) {
-    const error = document.getElementById(errorId);
+function checkPasswordMatch(passwordId, confirmPasswordId) {
+    const password = document.getElementById(passwordId);
+    const confirmPassword = document.getElementById(confirmPasswordId);
+    const error = confirmPassword.nextElementSibling;
     if (password.value.trim() !== confirmPassword.value.trim()) {
         error.textContent = 'Hasła nie pasują do siebie';
         return false;
@@ -81,8 +114,9 @@ function checkPasswordMatch(password, confirmPassword, errorId) {
     }
 }
 
-function checkPhone(input, errorId) {
-    const error = document.getElementById(errorId);
+function checkPhone(inputId) {
+    const input = document.getElementById(inputId);
+    const error = input.nextElementSibling;
     const phoneRegex = /^\d{9}$/;
     if (!phoneRegex.test(input.value.trim())) {
         error.textContent = 'Podaj poprawny numer telefonu (9 cyfr)';
@@ -93,17 +127,28 @@ function checkPhone(input, errorId) {
     }
 }
 
-function checkBirthdate(input, errorId) {
-    const error = document.getElementById(errorId);
-    const birthdate = new Date(input.value);
-    const currentDate = new Date();
-    const eighteenYearsAgo = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
-
-    if (birthdate >= eighteenYearsAgo) {
+function checkBirthdate(inputId) {
+    const input = document.getElementById(inputId);
+    const error = input.nextElementSibling;
+    const birthdate = input.value;
+    if (!checkAge(birthdate)) {
         error.textContent = 'Musisz być pełnoletni/a';
         return false;
     } else {
         error.textContent = '';
         return true;
+    }
+}
+
+function checkAge(birthdate) {
+    const currentDate = new Date();
+    const eighteenYearsAgo = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
+
+    const inputDate = new Date(birthdate);
+    
+    if (inputDate >= eighteenYearsAgo) {
+        return false; // Osoba jest za młoda
+    } else {
+        return true; // Osoba jest pełnoletnia
     }
 }
